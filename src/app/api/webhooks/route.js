5403d0a307bd99fbab3bb324e261eef78dca3946
Min La Wee Chan <u6520189@au.edu>
2025-06-21 15:04:04 +0700

    fix: update Clerk API method for user metadata in webhook

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -28,7 +28,7 @@ export async function POST(req) {
         );
         if (user && eventType === 'user.created') {
           try {
-            await clerkClient.users.updateUserMetadata(id, {
+            await clerkClient.users.updateUser(id, {
               publicMetadata: {
                 userMongoId: user._id,
                 isAdmin: user.isAdmin,
